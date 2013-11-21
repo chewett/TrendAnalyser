083370ae6fd266f6ac2006d0f64aa0e1736bbdf9
Christopher Hewett <chewett@hotmail.co.uk>
2013-11-21 11:22:07 +0000

    Added a file to download trend data from a specific world id

---
+++ download_trend_list.py
@@ -0,0 +1,22 @@
+import json
+from time import gmtime, strftime
+import datetime
+import sys
+
+from TrendAnalyser import TrendAnalyser
+
+if len(sys.argv) == 2:
+    woeid = int(sys.argv[1])
+else:
+    woeid = 1 #worldwide
+
+TA = TrendAnalyser()
+response = TA.api.request("trends/place", {"id": woeid})
+response_json = json.loads(response.text)
+
+
+TA.save_data(response_json, "trends/place/woeid_" + str(woeid))
+
+print "Time: " + str(datetime.datetime.now())
+print "Got trends for place woeid: " + str(woeid)
+
