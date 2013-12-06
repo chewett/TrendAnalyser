0b40521864337f8fdad6b01ac5885587561baeb8
Christopher Hewett <chewett@hotmail.co.uk>
2013-12-06 00:22:51 +0000

    Updated Stream Filter to use the new TA method

--- stream_filter.py
+++ stream_filter.py
@@ -5,17 +5,6 @@ import sys
 
 from TrendAnalyser import TrendAnalyser
 
-if len(sys.argv) == 2:
-    keywords = sys.argv[1]
-else:
-    print "Cannot find keywords to filter and download"
-    exit()
 
 TA = TrendAnalyser()
-response =TA.api.request("statuses/filter", {"track": keywords})
-
-print "Starting to download streaming data matching:", keywords
-
-for item in response.get_iterator():
-    TA.save_twitter_filter_data(item)
-
+TA.start_stream_filter()
