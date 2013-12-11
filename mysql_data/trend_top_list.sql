0ef91b9004c0704e5530d181f819d0824b4b3d73
Christopher Hewett <chewett@hotmail.co.uk>
2013-12-11 23:56:21 +0000

    Fixed minor spelling issue

--- mysql_data/trend_top_list.sql
+++ mysql_data/trend_top_list.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `trend_top_list` (
     `trend_top_list_id` int(10) unsigned NOT NULL,
-    `weoid` int(11) NOT NULL DEFAULT '0',
+    `woeid` int(11) NOT NULL DEFAULT '0',
     `as_of` varchar(45) NOT NULL DEFAULT '',
     `created_at` varchar(45) NOT NULL DEFAULT '',
     PRIMARY KEY (`trend_top_list_id`)
