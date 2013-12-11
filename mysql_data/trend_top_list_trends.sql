0eb5c33dbe826f8d26ffa5ed7d266c4fa99d25c3
Christopher Hewett <chewett@hotmail.co.uk>
2013-12-11 02:57:07 +0000

    Added trend_top_list_trends sql creation

---
+++ mysql_data/trend_top_list_trends.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `trend_top_list_trends` (
+    `trend_top_list_id` int(10) unsigned NOT NULL,
+    `name` varchar(45) NOT NULL,
+    `events` varchar(45) NOT NULL DEFAULT '',
+    `promoted_content` varchar(45) NOT NULL DEFAULT '',
+    PRIMARY KEY (`trend_top_list_id`,`name`),
+    CONSTRAINT `trend_top_list_id` FOREIGN KEY (`trend_top_list_id`) REFERENCES `trend_top_list` (`trend_top_list_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
