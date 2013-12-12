37fc6da171539cf76a753f501d45c255e47c2414
Christopher Hewett <chewett@hotmail.co.uk>
2013-12-12 00:28:11 +0000

    Added utf8 and missing autoincrement

--- mysql_data/trend_top_list.sql
+++ mysql_data/trend_top_list.sql
@@ -1,8 +1,7 @@
 CREATE TABLE `trend_top_list` (
-    `trend_top_list_id` int(10) unsigned NOT NULL,
+    `trend_top_list_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     `woeid` int(11) NOT NULL DEFAULT '0',
     `as_of` varchar(45) NOT NULL DEFAULT '',
     `created_at` varchar(45) NOT NULL DEFAULT '',
     PRIMARY KEY (`trend_top_list_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
