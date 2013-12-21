0a7cfc86c33b29d415e84e59db760c9d8cbb9c33
Christopher Hewett <chewett@hotmail.co.uk>
2013-12-21 23:57:20 +0000

    Added a start to the static HTML pages

--- web_interface.py
+++ web_interface.py
@@ -1,6 +1,13 @@
-from bottle import Bottle, route
+import os
+
+from bottle import Bottle, route, static_file
 
 app = Bottle()
+PATH = os.path.dirname(os.path.abspath(__file__))
+
+@route("/<filename>")
+def static_resource(filename):
+    return static_file("/static/"+filename, root=PATH)
 
 @route("/")
 def index_page():
