bffc239db118e4605b3165926b99d91913910afb
Christopher Hewett <chewett@hotmail.co.uk>
2013-12-22 19:31:30 +0000

    Added ability to pass tweet object as a dict instead of json string

--- StreamMessage.py
+++ StreamMessage.py
@@ -3,7 +3,11 @@ import json
 class StreamMessage:
 
     def __init__(self, tweet_data):
-        self.data = json.loads(tweet_data)
+        try: #assume they are passing in json
+            self.data = json.loads(tweet_data)
+        #if not json, assume its a dict and use it as the data
+        except TypeError:
+            self.data = tweet_data
 
     def get_type(self):
         try:
