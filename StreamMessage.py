2381a11025fc28599b0841f469f37a053052d0bd
Christopher Hewett <chewett@hotmail.co.uk>
2013-12-22 22:03:54 +0000

    Added ability to choose how it deals with disconnected messages

--- StreamMessage.py
+++ StreamMessage.py
@@ -2,12 +2,14 @@ import json
 
 class StreamMessage:
 
-    def __init__(self, tweet_data):
+    def __init__(self, tweet_data, disconnect_closes=True):
         try: #assume they are passing in json
             self.data = json.loads(tweet_data)
         #if not json, assume its a dict and use it as the data
         except TypeError:
             self.data = tweet_data
+
+        self.disconnect_closes = disconnect_closes
 
     def get_type(self):
         try:
@@ -18,10 +20,13 @@ class StreamMessage:
 
         try:
             if "disconnect" in self.data:
-                print "We have loaded a disconnect object and will close the program"
-                print "Disconnect message payload:"
-                print self.data['disconnect']
-                exit()
+                if self.disconnect_closes:
+                    print "We have loaded a disconnect object and will close the program"
+                    print "Disconnect message payload:"
+                    print self.data['disconnect']
+                    exit()
+                else:
+                    return "disconnect"
         except KeyError:
             pass
 
