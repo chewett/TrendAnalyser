7812b1a95a783a9eab37adea66f6eac2283ed8bc
Christopher Hewett <chewett@hotmail.co.uk>
2013-12-22 01:30:34 +0000

    Added json method to get the filter details

--- web_interface.py
+++ web_interface.py
@@ -1,9 +1,16 @@
 import os
 
 from bottle import Bottle, route, static_file
+from WookieDb import WookieDb
+from TrendAnalyser import TrendAnalyser
 
 app = Bottle()
 PATH = os.path.dirname(os.path.abspath(__file__))
+TA = TrendAnalyser()
+
+@route("/filters.json")
+def filters_json():
+    return {"res" : TA._get_filter_keywords()}
 
 @route("/<filename>")
 def static_resource(filename):
