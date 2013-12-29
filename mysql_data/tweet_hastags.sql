7f92d751766251fc7ff288b85add1650123cc8a0
Christopher Hewett <chewett@hotmail.co.uk>
2013-12-29 01:25:42 +0000

    hashtag length being truncated, changing to see if this fixes it

--- mysql_data/tweet_hastags.sql
+++ mysql_data/tweet_hastags.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `tweet_hashtags` (
     `tweetId` bigint(20) NOT NULL,
-    `hashtag` varchar(140) NOT NULL,
+    `hashtag` varchar(180) NOT NULL,
     PRIMARY KEY (`tweetId`,`hashtag`),
     CONSTRAINT `tweet_id` FOREIGN KEY (`tweetId`) REFERENCES `tweet_details` (`tweetId`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
