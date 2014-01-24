f13b22c45d3ba5c960299a738b0588b15a8952bf
Christopher Hewett <chewett@hotmail.co.uk>
2014-01-24 17:28:05 +0000

    Added woeid table creation sql

---
+++ mysql_data/woeid_data.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `woeid_data` (
+    `woeid` int(11) NOT NULL,
+    `country` varchar(50) DEFAULT NULL,
+    `countryCode` varchar(5) DEFAULT NULL,
+    `name` varchar(50) DEFAULT NULL,
+    `parentWoeid` int(11) DEFAULT NULL,
+    `placeCode` int(11) DEFAULT NULL,
+    `placeName` varchar(50) DEFAULT NULL,
+    `url` varchar(254) DEFAULT NULL,
+    PRIMARY KEY (`woeid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
