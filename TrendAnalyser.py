52e1f4c47111d83f60c9a2ec9d09a6df41e35267
Christopher Hewett <chewett@hotmail.co.uk>
2014-01-31 16:46:55 +0000

    Added saving timestamp to data

--- TrendAnalyser.py
+++ TrendAnalyser.py
@@ -62,7 +62,9 @@ class TrendAnalyser:
 
         if msg.get_type() == "tweet":
             if msg.data['entities']['hashtags'] != []:
-                tweet_details = {"tweetId" : msg.data['id'], "created_at" : msg.data['created_at']}
+                tweet_details = {"tweetId" : msg.data['id'],
+                                 "created_at" : msg.data['created_at'],
+                                 "created_at_new" : calendar.timegm(parser.parse(msg.data['created_at']).utctimetuple())}
                 try:
                     self.db.insert("tweet_details", tweet_details)
                 except _mysql_exceptions.IntegrityError as e:
