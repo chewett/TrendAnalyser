c8cbf59aa29a73d0fb9f72c8e2a696c16c62468a
Christopher Hewett <chewett@hotmail.co.uk>
2014-02-01 11:43:41 +0000

    Modified table structure to hold time in a nicer format

--- mysql_data/trend_top_list.sql
+++ mysql_data/trend_top_list.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `trend_top_list` (
     `trend_top_list_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `woeid` int(11) NOT NULL DEFAULT '0',
-    `as_of` varchar(45) NOT NULL DEFAULT '',
-    `created_at` varchar(45) NOT NULL DEFAULT '',
+    `woeid` int(11) NOT NULL,
+    `as_of` int(11) NOT NULL,
+    `created_at` int(11) NOT NULL,
     PRIMARY KEY (`trend_top_list_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- mysql_data/tweet_details.sql
+++ mysql_data/tweet_details.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `tweet_details` (
     `tweetId` bigint(20) NOT NULL,
-    `created_at` varchar(45) NOT NULL,
+    `created_at` int(11) NOT NULL,
     PRIMARY KEY (`tweetId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
