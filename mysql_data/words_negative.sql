16badc3ebe701bf8ad5f0c12687d0dee61050a97
Christopher Hewett <chewett@hotmail.co.uk>
2014-02-01 13:19:25 +0000

    Added tables to store positive and negative words

---
+++ mysql_data/words_negative.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `words_negative` (
+    `word` varchar(100) NOT NULL,
+    PRIMARY KEY (`word`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8
---
+++ mysql_data/words_positive.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `words_positive` (
+  i  `word` varchar(100) NOT NULL,
+    PRIMARY KEY (`word`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8
