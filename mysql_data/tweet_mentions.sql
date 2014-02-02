caf5d80fab9b3131ebc8ba5b95043ce568ea0163
Christopher Hewett <chewett@hotmail.co.uk>
2014-02-02 21:43:32 +0000

    Added new table tweet_mentions

---
+++ mysql_data/tweet_mentions.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `tweet_mentions` (
+    `tweetId` bigint(20) NOT NULL,
+    `user_id` int(11) unsigned NOT NULL,
+    `name` varchar(150) NOT NULL,
+    `screen_name` varchar(150) NOT NULL,
+    PRIMARY KEY (`tweetId`),
+    KEY `fk_tweet_mentions_1` (`tweetId`),
+    CONSTRAINT `fk_tweet_mentions_1` FOREIGN KEY (`tweetId`) REFERENCES `tweet_details` (`tweetId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
