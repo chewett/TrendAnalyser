620730b18b9cd4edf7d77f310cb0f58cdb51d318
Christopher Hewett <chewett@hotmail.co.uk>
2014-02-02 20:31:59 +0000

    Added semantic analysis of data

--- TrendAnalyser.py
+++ TrendAnalyser.py
@@ -66,7 +66,15 @@ class TrendAnalyser:
 
         if msg.get_type() == "tweet":
             if msg.data['entities']['hashtags'] != []:
+                words = msg.data['text'].split(" ")
+                where_clause = "WHERE word in (\"" + "\",\"".join(words) + "\");"
+
+                positive = self.db.select("words_positive", "count(*)", where_clause)[0]["count(*)"]
+                negative = self.db.select("words_negative", "count(*)", where_clause)[0]["count(*)"]
+
                 tweet_details = {"tweetId" : msg.data['id'],
+                                 "positive" : positive,
+                                 "negative" : negative,
                                  "created_at" : self._convert_to_unix(msg.data['created_at'])}
                 try:
                     self.db.insert("tweet_details", tweet_details)
