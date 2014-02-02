84871e4041e6609faa24fe456e1efcd375d507a4
Christopher Hewett <chewett@hotmail.co.uk>
2014-02-02 21:50:21 +0000

    Fixed error where primary key was only on tweetId

--- mysql_data/tweet_mentions.sql
+++ mysql_data/tweet_mentions.sql
@@ -3,7 +3,7 @@ CREATE TABLE `tweet_mentions` (
     `user_id` int(11) unsigned NOT NULL,
     `name` varchar(150) NOT NULL,
     `screen_name` varchar(150) NOT NULL,
-    PRIMARY KEY (`tweetId`),
+    PRIMARY KEY (`tweetId`,`user_id`),
     KEY `fk_tweet_mentions_1` (`tweetId`),
     CONSTRAINT `fk_tweet_mentions_1` FOREIGN KEY (`tweetId`) REFERENCES `tweet_details` (`tweetId`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
