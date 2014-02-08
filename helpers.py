9e74050bbe11a28b3cfdc1fc356dfba981fe4263
Christopher Hewett <chewett@hotmail.co.uk>
2014-02-08 20:10:26 +0000

    Removed self from the function signitures

--- helpers.py
+++ helpers.py
@@ -3,11 +3,11 @@ import errno
 from dateutil import parser
 import calendar
 
-def convert_to_unix(self, timestamp):
+def convert_to_unix(timestamp):
     '''Used to convert any timestamp format to unix time'''
     return calendar.timegm(parser.parse(timestamp).utctimetuple())
 
-def save_data(self, json_data, location):
+def save_data(json_data, location):
     '''Saves json data to a specific file location'''
     try:
         os.makedirs(os.path.dirname(location))
