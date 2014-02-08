a2820df22c9759353f43d8817dbbaa5922c5c9c1
Christopher Hewett <chewett@hotmail.co.uk>
2014-02-08 19:12:12 +0000

    Added the basic options to the file

--- mysql_data/options.sql
+++ mysql_data/options.sql
@@ -2,4 +2,8 @@ CREATE TABLE `options` (
     `key` varchar(255) NOT NULL,
     `value` varchar(255) DEFAULT NULL,
     PRIMARY KEY (`key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `options` (`key`,`value`) VALUES ('debug','false');
+INSERT INTO `options` (`key`,`value`) VALUES ('offline','false');
+INSERT INTO `options` (`key`,`value`) VALUES ('save_data_location','/home/user/tweet_data/');
+INSERT INTO `options` (`key`,`value`) VALUES ('twitter_key_location','/home/user/twitter.json');
