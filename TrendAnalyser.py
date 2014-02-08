a19f7e886311437b79fcb82bbb81be6e099e7e7c
Christopher Hewett <chewett@hotmail.co.uk>
2014-02-08 19:38:34 +0000

    Added Comments to all methods

--- TrendAnalyser.py
+++ TrendAnalyser.py
@@ -15,6 +15,7 @@ import _mysql_exceptions
 class TrendAnalyser:
 
     def __init__(self, config_location="conf.json", load_api=True, load_db=True):
+        '''Initialises the TrendAnalyser class object'''
         self.load_conf(config_location)
         if load_db:
             self.connect_to_db()
@@ -24,6 +25,7 @@ class TrendAnalyser:
             self.load_api()
 
     def connect_to_db(self):
+        '''Connects to the database using the details in conf'''
         self.db = WookieDb(self.conf['database_host'],
                            self.conf['database_username'],
                            self.conf['database_password'],
@@ -31,11 +33,13 @@ class TrendAnalyser:
                            select_type='dict', charset="utf8")
 
     def load_conf(self, config_location):
+        '''Loads the config from the given conf file location'''
         self.conf = {"debug" : False,
                      "offline": False}
         self.conf.update(json.load(open(config_location)))
 
     def load_db_conf(self):
+        '''Accesses the DB and loads all the config details into the object'''
         db_options = self.db.select("options", "*")
         for option in db_options:
             value = option["value"]
@@ -47,6 +51,7 @@ class TrendAnalyser:
             self.conf[option['key']] = value
 
     def load_api(self):
+        '''creates an instance of the TwitterAPI class to use'''
         details = json.load(open(self.conf['twitter_key_location']))
 
         self.api = TwitterAPI(details['consumer_key'],
@@ -55,15 +60,18 @@ class TrendAnalyser:
                               details['access_token_secret'])
 
     def _convert_to_unix(self, timestamp):
+        '''Used to convert any timestamp format to unix time'''
         return calendar.timegm(parser.parse(timestamp).utctimetuple())
 
     def save_trend_data(self, json_data, woeid):
+        '''Saves Twitter Trend data to a json file'''
         filelocation = os.path.join(self.conf['save_data_location'],
                                 "trends/place/weoid_" + str(woeid) + "_" +
                                 str(int(time.time())) + ".json")
         self.save_data(json_data, filelocation)
 
     def save_twitter_sample_data(self, json_data):
+        '''Saves data from the sample API to json files'''
         time_folder = int(time.time() / 300) * 300
 
         filelocation = os.path.join(self.conf['save_data_location'],
@@ -74,6 +82,7 @@ class TrendAnalyser:
         self.save_data(json_data, filelocation)
 
     def save_sample_data_db(self, json_data):
+        '''Saves as little as possible from the sample API to the database'''
         msg = StreamMessage(json_data)
 
         if msg.get_type() == "tweet":
@@ -122,6 +131,7 @@ class TrendAnalyser:
                 self.db.commit()
 
     def save_twitter_filter_data(self, json_data):
+        '''Saves the given json data into the filter directory'''
         time_folder = int(time.time() / 300) * 300
 
         filelocation = os.path.join(self.conf['save_data_location'],
@@ -132,6 +142,7 @@ class TrendAnalyser:
         self.save_data(json_data, filelocation)
 
     def start_stream_filter(self):
+        '''Gets the filter terms to search for them starts downloading them'''
         terms = self._get_filter_keywords()
 
         print "Loading terms from database"
@@ -155,6 +166,7 @@ class TrendAnalyser:
                 items = 0
 
     def _get_filter_keywords(self):
+        '''Get all the filter keywords from the database'''
         terms = self.db.select("filter_status_terms", "*")
         combined_terms = []
         for term in terms:
@@ -163,6 +175,7 @@ class TrendAnalyser:
         return combined_terms
 
     def _get_latest_trends(self):
+        '''Gets the latest trending topics from each woeid'''
         woeids = self.db.select("trend_top_list", "distinct(woeid)")
         latest_trends = {}
         for row in woeids:
@@ -185,6 +198,7 @@ class TrendAnalyser:
         return latest_trends
 
     def _get_trending_details(self, search_term):
+        '''Gets all trending information for a specific search term'''
         details = self.db.select("trend_top_list l "+
                                  "left join trend_top_list_trends t on t.trend_top_list_id = l.trend_top_list_id " +
                                  "left join woeid_data w on l.woeid = w.woeid",
@@ -194,12 +208,14 @@ class TrendAnalyser:
         return search_details
 
     def _get_hashtag_details(self, search_term):
+        '''Gets the details about a specific hashtag from the database'''
         details = self.db.select("tweet_hashtags h left join tweet_details d on h.tweetId = d.tweetId", "d.*",
                                  "WHERE hashtag = '"+ search_term +"'")
         search_details = {'hashtags' : details, 'hashtag' : search_term}
         return search_details
 
     def _get_hashtag_frequency(self, search_term):
+        '''Returns data about a hashtag's popularity over time'''
         time_period = 86400# seconds in a day
 
         details = self._get_hashtag_details(search_term)
@@ -232,6 +248,7 @@ class TrendAnalyser:
         return data
 
     def _get_mention_details(self, search_term):
+        '''Returns data about a specific mention screenname'''
         details = self.db.select("tweet_mentions m left join tweet_details d on m.tweetId = d.tweetId",
                                  "d.*",
                                  "WHERE screen_name = '" + search_term +"'")
@@ -239,6 +256,7 @@ class TrendAnalyser:
         return search_details
 
     def _get_mention_frequency(self, search_term):
+        '''Returns data about a mention's popularity over time'''
         time_period = 86400# seconds in a day
 
         details = self._get_mention_details(search_term)
@@ -270,14 +288,17 @@ class TrendAnalyser:
         return data
 
     def _get_trending_woeids_downloading(self):
+        '''Gets the details of woeids you want to download from the database'''
         return self.db.select("woeids_download d left join woeid_data w on d.woeid = w.woeid", "d.*, w.name")
 
     def download_trends(self):
+        '''Downloads all the woeid data from the ones you want to download'''
         woeids = self.db.select("woeids_download", "*")
         for woeid in woeids:
             self.download_trend_list(woeid['woeid'])
 
     def download_trend_list(self, woeid):
+        '''Saves one woeid data to the data'''
         response = self.api.request("trends/place", {"id" : woeid})
         response_json = json.loads(response.text)[0]
 
@@ -302,6 +323,7 @@ class TrendAnalyser:
         self.db.connection.commit()
 
     def save_data(self, json_data, location):
+        '''Saves json data to a specific file location'''
         try:
             os.makedirs(os.path.dirname(location))
         except OSError as e:
@@ -311,6 +333,7 @@ class TrendAnalyser:
         json.dump(json_data, open(location, 'w'))
 
     def _update_woeid_data(self):
+        '''Downloads all the avalible woeid data from Twitter and stores it'''
         response = self.api.request("trends/available")
         response_json = json.loads(response.text)
 
@@ -329,18 +352,22 @@ class TrendAnalyser:
         self.db.commit()
 
     def _get_woeid_data(self):
+        '''Gets all woeid data from the database'''
         details = self.db.select("woeid_data", "*")
         return details
 
     def _get_words_positive(self):
+        '''Gets all the positive words from the database'''
         details = self.db.select("words_positive", "*")
         return details
 
     def _get_words_negative(self):
+        '''Gets all the negative words from the database'''
         details = self.db.select("words_negative", "*")
         return details
 
     def set_option(self, key, value):
+        '''Allows you to set database options'''
         existing = self.db.select("options", "*", " WHERE options.key = '" + key + "' LIMIT 1;")
         if existing:
             self.db.update("options", {"value" : value}, "WHERE options.key = '" + key + "' LIMIT 1;")
