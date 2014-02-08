85431ed98546195b56e238421cb2cb80c4d15d44
Christopher Hewett <chewett@hotmail.co.uk>
2014-02-08 19:45:04 +0000

    Removed unused imports and fixed space issue

--- stream_samples.py
+++ stream_samples.py
@@ -1,12 +1,7 @@
-import json
-from time import gmtime, strftime
-import datetime
-import sys
-
 from TrendAnalyser import TrendAnalyser
 
 TA = TrendAnalyser()
-response =TA.api.request("statuses/sample")
+response = TA.api.request("statuses/sample")
 
 print "Starting to download streaming sample data"
 
