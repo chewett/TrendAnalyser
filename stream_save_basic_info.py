9a84d06077e48a7d26ad50d32ead6d622fb2cd53
Christopher Hewett <chewett@hotmail.co.uk>
2014-02-08 19:45:37 +0000

    Removed unused imports and fixed space issue

--- stream_save_basic_info.py
+++ stream_save_basic_info.py
@@ -1,12 +1,7 @@
-import json
-from time import gmtime, strftime
-import datetime
-import sys
-
 from TrendAnalyser import TrendAnalyser
 
 TA = TrendAnalyser()
-response =TA.api.request("statuses/sample")
+response = TA.api.request("statuses/sample")
 
 for item in response.get_iterator():
     TA.save_sample_data_db(item)
