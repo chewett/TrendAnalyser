341c724a90da07ed4b9fd444c788bb9902c539a2
Christopher Hewett <chewett@hotmail.co.uk>
2014-02-08 14:34:12 +0000

    Added a file to set the option of a variable

---
+++ set_option.py
@@ -0,0 +1,15 @@
+import sys
+
+from TrendAnalyser import TrendAnalyser
+
+if len(sys.argv) == 3:
+    key = sys.argv[1]
+    value = sys.argv[2]
+else:
+    print "You must include a key and value to insert/modify this option"
+    exit()
+
+TA = TrendAnalyser(load_api=False)
+TA.set_option(key, value)
+
+print "Set the value of " + key + " to " + value
