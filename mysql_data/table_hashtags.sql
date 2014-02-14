6f1a8e18afac515b4cc3ad4d39c043b229d22e51
Christopher Hewett <chewett@hotmail.co.uk>
2014-02-14 16:56:51 +0000

    Formatted sql to match other SQL

--- mysql_data/table_hashtags.sql
+++ mysql_data/table_hashtags.sql
@@ -2,6 +2,5 @@ CREATE  TABLE `trendanalyser`.`hashtags` (
     `hid` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
     `hashtag` VARCHAR(255) NOT NULL ,
     PRIMARY KEY (`hid`) ,
-    UNIQUE INDEX `hashtag_UNIQUE` (`hashtag` ASC) )
-ENGINE = MyISAM
-DEFAULT CHARACTER SET = utf8;
+    UNIQUE INDEX `hashtag_UNIQUE` (`hashtag` ASC)
+)ENGINE = MyISAM DEFAULT CHARACTER SET = utf8;
