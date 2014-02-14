532a0c0a529c756edb748139e6dd322107530e52
Christopher Hewett <chewett@hotmail.co.uk>
2014-02-14 17:43:04 +0000

    Added the new tweet mentions db

--- mysql_data/table_tweet_mentions.sql
+++ mysql_data/table_tweet_mentions.sql
@@ -1,8 +1,6 @@
 CREATE TABLE `tweet_mentions` (
     `tweetId` bigint(20) NOT NULL,
-    `user_id` int(11) unsigned NOT NULL,
-    `name` varchar(150) NOT NULL,
-    `screen_name` varchar(150) NOT NULL,
-    PRIMARY KEY (`tweetId`,`user_id`),
+    `mid` int(11) unsigned NOT NULL,
+    PRIMARY KEY (`tweetId`,`mid`),
     KEY `fk_tweet_mentions_1` (`tweetId`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
