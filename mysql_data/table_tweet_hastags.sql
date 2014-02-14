8232f03e4b783d2cdfd76e364584c2481ec39ed1
Christopher Hewett <chewett@hotmail.co.uk>
2014-02-14 22:59:02 +0000

    Fixed error with table creation

--- mysql_data/table_tweet_hastags.sql
+++ mysql_data/table_tweet_hastags.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `tweet_hashtags` (
     `tweetId` bigint(20) NOT NULL,
     `hid` int(11) NOT NULL,
-    PRIMARY KEY (`tweetId`,`hashtag`)
+    PRIMARY KEY (`tweetId`,`hid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
